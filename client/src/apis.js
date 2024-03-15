cdd9d804d0eccf1ca4b6b096d00a3cfb81e57ed4
sanjay prajapat <sanjayprajapat68149@gmail.com>
2024-03-15 08:01:02 +0530

    new chnages

--- client/src/apis.js
+++ client/src/apis.js
@@ -1,2 +1,2 @@
-export const baseUrl = 'http://localhost:8000/api/v1/';
-// export const baseUrl = 'https://advisoropedia-htmg.onrender.com/api/v1/';
+// export const baseUrl = 'http://localhost:8000/api/v1/';
+export const baseUrl = 'https://loan-app-cu5r.onrender.com/api/v1/';
